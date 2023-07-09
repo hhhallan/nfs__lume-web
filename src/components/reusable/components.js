ce2ea673f12e4b15ea078e7436b9d90177df9109
hhhallan <allanstm76@gmail.com>
2023-07-09 21:50:06 +0200

    site fini

--- src/components/reusable/components.js
+++ src/components/reusable/components.js
@@ -1,6 +1,12 @@
 import ProtectedRoute from "./ProtectedRoute.jsx";
+import Title from "./Title.jsx";
+import HeaderCard from "../pages/home/HeaderCard.jsx";
+import List from "./List.jsx";
 
 
 export {
     ProtectedRoute,
+    Title,
+    HeaderCard,
+    List,
 }
